b7735ce39b81f911535e6820dccca9edc1810a38
Wagner Roberto <wagnerrsoliveira@gmail.com>
2018-08-12 20:00:50 -0300

    Iniciando Projeto Teste de Desenvolvimento

---
+++ src/main/java/br/com/pizzariauds/PizzariaUdsApplication.java
@@ -0,0 +1,19 @@
+package br.com.pizzariauds;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaUdsApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaUdsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+	}
+}
